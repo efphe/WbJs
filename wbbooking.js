681a85cb3bfcafdae8f5ec955e4f63f6dc6ae2d8
yellow <yellow@wubook.net>
2010-11-01 02:14:20 +0100

    Files and a little bit of docs

---
+++ wbbooking.js
@@ -0,0 +1,28 @@
+/*
+ * I populate the form on Hotel sites and normally
+ * I'm loaded by wbloader.js.
+ *
+ * I use the following libs/css files:
+ *
+ *  http://wubook.net/css/wbredcal.css
+ *  http://wubook.net/js/wu_cals.js
+ *  http://wubook.net/css/wbk.css
+ *  jQuery (http://wubook.net/js/jquery.js)
+ */
+
+
+/* 
+ * What's button? I don't know: maybe an url with
+ * an image, or text? Or, some way, both?
+ */
+
+/*
+ * Def values:
+ *
+ *  dates    ~> true
+ *  deletion ~> true
+ *  defland  ~> 'it'
+ *  button   ~> ????
+ */
+function initWuBook(dates, deletion, deflang, button) {
+}
---
+++ wbloader.js
@@ -0,0 +1,5 @@
+/*
+ * I load the js framework dynamically: once loaded,
+ * I'll populate the online booking form.
+ *
+ */
